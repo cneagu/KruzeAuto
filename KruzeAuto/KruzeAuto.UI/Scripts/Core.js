bff597d63169c50986b5ee96e76cbdc44fa08108
Cristian Stefan Neagu <neagucristianstefan@yahoo.com>
2018-01-08 15:36:04 +0200

    Epic Fail V1.0

---
+++ KruzeAuto/KruzeAuto.UI/Scripts/Core.js
@@ -0,0 +1,54 @@
+﻿var brand = ["BMW",
+    "Mercedes",
+    "Audi"
+];
+var bmw = ["1 Series",
+    "3 Series",
+    "5 Series",
+    "6 Series",
+    "7 Series",
+    "X1",
+    "X3",
+    "X6",
+    "M3",
+    "M5",
+    "M6"
+];
+var mercedes = ["A Class",
+    "B Class",
+    "C Class",
+    "E Class",
+    "S Class",
+    "CLS",
+    "GT AMG"
+];
+var audi = ["A1",
+    "A2",
+    "A3",
+    "A4",
+    "A5",
+    "A6",
+    "A7",
+    "A8",
+    "Q7"
+];
+
+
+$(document).ready(function () {
+
+    for (var i = 0; i <= brand.length; i++) {
+        $('#brands').append("<option value='" + brand[i] + "'>");
+    }
+
+
+    $("form").submit(function (event) {
+
+
+        $("input[name='brands']").on('input', function (e) {
+            alert($(this).val());
+        });
+       
+
+    });
+});
+
---
+++ KruzeAuto/KruzeAuto.UI/Scripts/Global_Variables.js
@@ -0,0 +1,34 @@
+﻿var brand = ["BMW",
+            "Mercedes",
+            "Audi"
+];
+var bmw = ["1 Series",
+            "3 Series",
+            "5 Series",
+            "6 Series",
+            "7 Series",
+            "X1",
+            "X3",
+            "X6",
+            "M3",
+            "M5",
+            "M6"
+];
+var mercedes = ["A Class",
+                "B Class",
+                "C Class",
+                "E Class",
+                "S Class",
+                "CLS",
+                "GT AMG"
+];
+var audi = ["A1",
+    "A2",
+    "A3",
+    "A4",
+    "A5",
+    "A6",
+    "A7",
+    "A8",
+    "Q7"
+];
