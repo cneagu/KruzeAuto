f12c3327cdf228a11930a833fa6a70a892eb8f86
Cristian Stefan Neagu <neagucristianstefan@yahoo.com>
2018-01-10 01:02:30 +0200

    spa fail v1.0

---
+++ KruzeAuto/KruzeAuto.UI/Scripts/Login_Singin.js
@@ -0,0 +1,19 @@
+﻿var user1 = {
+    UserID: '561FC9B6-74AB-D604-337F-3D83F8504E01',
+    Email: 'neagucristianstefan@yahoo.com',
+    UserName: 'neagucristianstefan',
+    Password: 'UMT365',
+    PhoneNumber: '0765537186',
+    CreationDate: '2017-12-01 21:33',
+    Subscribed: '1'
+};
+
+var user2 = {
+    UserID: 'E5C24E7A-A819-1E62-E37C-0700402A1B8C',
+    Email: 'felis.ullamcorper.viverra@Suspendissenonleo.edu',
+    UserName: 'felis',
+    Password: 'vivera123',
+    PhoneNumber: '0762561832',
+    CreationDate: '1996-07-13 12:13',
+    Subscribed: ''
+};
--- KruzeAuto/KruzeAuto.UI/Scripts/SPA.js
+++ KruzeAuto/KruzeAuto.UI/Scripts/SPA.js
@@ -1,1 +1,65 @@
-﻿
+﻿(function () {
+
+    // Stores the cached partial HTML pages.
+    // Keys correspond to fragment identifiers.
+    // Values are the text content of each loaded partial HTML file.
+    var partialsCache = {}
+
+    // Gets the appropriate content for the given fragment identifier.
+    // This function implements a simple cache.
+    function getContent(fragmentId, callback) {
+
+        // If the page has been fetched before,
+        if (partialsCache[fragmentId]) {
+
+            // pass the previously fetched content to the callback.
+            callback(partialsCache[fragmentId]);
+
+        } else {
+            // If the page has not been fetched before, fetch it.
+            $.get(fragmentId + ".html", function (content) {
+
+                // Store the fetched content in the cache.
+                partialsCache[fragmentId] = content;
+
+                // Pass the newly fetched content to the callback.
+                callback(content);
+            });
+        }
+    }
+
+    // Sets the "active" class on the active navigation link.
+    function setActiveLink(fragmentId) {
+        $(".page-content").each(function (i, divElement) {
+            var divElement = $(this).attr('id');
+            console.log(divElement);                
+            if (divElement != fragmentId) 
+                $(this).attr("hidden", "true");
+        });
+    }
+
+    // Updates dynamic content based on the fragment identifier.
+    function navigate() {
+
+        // Isolate the fragment identifier using substr.
+        // This gets rid of the "#" character.
+        var fragmentId = location.hash.substr(1);
+        
+
+        // Toggle the "active" class on the link currently navigated to.
+        setActiveLink(fragmentId);
+    }
+
+    // If no fragment identifier is provided,
+    if (!location.hash) {
+
+        // default to #home.
+        location.hash = "#home";
+    }
+
+    // Navigate once to the initial fragment identifier.
+    navigate();
+
+    // Navigate whenever the fragment identifier value changes.
+    $(window).bind('hashchange', navigate);
+}());
